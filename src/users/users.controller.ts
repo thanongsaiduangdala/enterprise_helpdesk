9549a3a8929edbba5a7fbf3b8a2cf62bb4923017
thanongsaiduangdala <thanongsai.duangdala@gmail.com>
2026-08-28 08:26:18 +0700

    asdsda

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,40 +4,41 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { PermissionsGuard } from '../common/guards/permissions.guard';
+import { RequirePermission } from '../common/decorators/require-permission.decorator';
 
 @Controller('users')
+@UseGuards(JwtAuthGuard, PermissionsGuard)
+@ApiBearerAuth()
 export class UsersController {
     constructor(private usersService: UsersService) { }
 
     @Post()
+    @RequirePermission('users', 'create')
     create(@Body() dto: CreateUserDto) {
         return this.usersService.create(dto);
     }
 
     @Get()
-    @UseGuards(JwtAuthGuard)
-    @ApiBearerAuth()
+    @RequirePermission('users', 'read')
     findAll() {
         return this.usersService.findAll();
     }
 
     @Get(':id')
-    @UseGuards(JwtAuthGuard)
-    @ApiBearerAuth()
+    @RequirePermission('users', 'read')
     findOne(@Param('id') id: string) {
         return this.usersService.findOne(id);
     }
 
     @Patch(':id')
-    @UseGuards(JwtAuthGuard)
-    @ApiBearerAuth()
+    @RequirePermission('users', 'update')
     update(@Param('id') id: string, @Body() dto: UpdateUserDto) {
         return this.usersService.update(id, dto);
     }
 
     @Delete(':id')
-    @UseGuards(JwtAuthGuard)
-    @ApiBearerAuth()
+    @RequirePermission('users', 'delete')
     remove(@Param('id') id: string) {
         return this.usersService.remove(id);
     }
